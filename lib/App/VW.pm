c977e896f716b19c0e6885cb7c9beeb0d513d59a
John Beppu <john.beppu@gmail.com>
2008-09-17 03:24:54 -0700

    removed template generated docs -- i have my own style

--- lib/App/VW.pm
+++ lib/App/VW.pm
@@ -1,106 +1,15 @@
 package App::VW;
 
-use warnings;
 use strict;
-
-=head1 NAME
-
-App::VW - The great new App::VW!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
+use warnings;
+use base 'App::CLI';
 
 our $VERSION = '0.01';
 
-
-=head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use App::VW;
-
-    my $foo = App::VW->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+1;
 
 =head1 AUTHOR
 
-John BEPPU, C<< <john.beppu at gmail.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-app-vw at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-VW>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc App::VW
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-VW>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/App-VW>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/App-VW>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/App-VW>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2008 John BEPPU, all rights reserved.
-
-This program is released under the following license: mit
-
+John BEPPU C<< <beppu at cpan.org> >>
 
 =cut
-
-1; # End of App::VW
