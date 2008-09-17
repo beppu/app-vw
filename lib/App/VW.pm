c9b50754fcc9967741b9e211fa2d1ff771aff256
John Beppu <john.beppu@gmail.com>
2008-09-17 04:00:54 -0700

    NAME section

--- lib/App/VW.pm
+++ lib/App/VW.pm
@@ -8,6 +8,14 @@ our $VERSION = '0.01';
 
 1;
 
+__END__
+
+=head1 NAME
+
+App::VW - a deployment system for Squatting+Continuity web apps
+
+=head1 DESCRIPTION
+
 =head1 AUTHOR
 
 John BEPPU C<< <beppu at cpan.org> >>
